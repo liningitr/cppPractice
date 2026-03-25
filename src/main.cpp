5b11fb5da9d264d4b1c1e93c0855e07e3e11b8fe
Ning Li <liningutk@gmail.com>
2026-03-24 23:47:34 -0700

    changed printing sequence to print CLI input variables first

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,6 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-    // Print a greeting message.
-    cout << "Hello, World!" << endl;
-    
     // Command-line inputs: argc is argument count, argv is argument array.
     cout << "Program name: " << (argc > 0 ? argv[0] : "(none)") << endl;
     cout << "Argument count: " << argc << endl;
@@ -14,6 +11,9 @@ int main(int argc, char* argv[]) {
     for (int i = 1; i < argc; ++i) {
         cout << "argv[" << i << "] = " << argv[i] << endl;
     }
+
+    // Print a greeting message.
+    cout << "Hello, World!" << endl;
 
     return 0;
 }
